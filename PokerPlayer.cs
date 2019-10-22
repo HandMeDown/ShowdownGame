6827044163b232bc99bff48df13747e166756d76
Tony <mcconvilletony@gmail.com>
2019-10-22 12:52:53 -0700

    Created inital program, game data file and PokerPlayer object.

---
+++ PokerPlayer.cs
@@ -0,0 +1,17 @@
+﻿namespace ShowdownGame
+{
+    public class PokerPlayer
+    {
+        public string Name;
+        public string Cards;
+        //private string[] Hand;
+
+        public PokerPlayer(string name, string cards)
+        {
+            this.Name = name;
+            this.Cards = cards;
+
+            var hand = cards.Split(',');
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ShowdownGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var gamePlayers = new List<PokerPlayer>();
+
+            string name = null;
+            bool gotName = false;
+            //read player data from file
+            foreach (string line in File.ReadLines(@"../../../AppData/gameData.txt"))
+            {
+
+                //create new player, grabbing name first followed by cards
+                if (!gotName)
+                {
+                    name = line;
+                    gotName = true;
+                    continue;
+                }
+
+               // Console.WriteLine(name + " " + line);
+
+                //create new PokerPlayer object
+                var newPlayer = new PokerPlayer(name, line);
+
+                //add newPlayer to list of gamePlayers
+                gamePlayers.Add(newPlayer);
+
+                name = null;
+                gotName = false;
+;                    
+            }
+            gamePlayers.ForEach(i => Console.Write("{0}\t {1}\t", i.Name, i.Cards));
+            Console.ReadLine();
+        }
+    }
+}
